cf50e762dba9940d64ae5bbb0152865fd71a1c52
xHorion <lyrchikov.tolya@gmail.com>
2026-01-17 19:23:52 +0100

    Last fix 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from "../views/HomeView.vue"
 import AboutView from '@/views/AboutView.vue'
 import ServiceView from '@/views/ServiceView.vue'
@@ -9,7 +9,7 @@ import ContactView from '@/views/ContactView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
